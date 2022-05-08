1d07dba00f4f1bfd9bb0c6c9b34e81be7af85012
Matikun <73756140+Matikun@users.noreply.github.com>
2022-05-08 15:42:26 -0300

    component common details aded

---
+++ components/ui/common/details/index.js
@@ -0,0 +1,58 @@
+import { messages } from '@messages';
+import { Descriptions, Tag } from 'antd';
+import { tx_status } from 'enums';
+
+const DetailCard = ({ data }) => {
+	const {
+		blockHash,
+		blockNumber,
+		from,
+		to,
+		gas,
+		txreceipt_status,
+		value,
+		gasPrice,
+		confirmations,
+		timeStamp,
+		hash,
+		nonce,
+	} = data;
+
+	const success = txreceipt_status === tx_status.success;
+
+	return (
+		<>
+			<Descriptions
+				column={{ xs: 1, md: 1, lg: 1, xl: 1 }}
+				bordered
+				title={messages.details.title}
+				labelStyle={{ fontWeight: 'bold' }}
+			>
+				<Descriptions.Item label={messages.details.txhash}>{hash}</Descriptions.Item>
+				<Descriptions.Item label={messages.details.blockhash}>{blockHash}</Descriptions.Item>
+				{txreceipt_status && (
+					<Descriptions.Item label={messages.details.status}>
+						{success ? (
+							<Tag color='success'>{messages.common.success}</Tag>
+						) : (
+							<Tag color='error'>{messages.common.error}</Tag>
+						)}
+					</Descriptions.Item>
+				)}
+				<Descriptions.Item label={messages.details.block}>{blockNumber}</Descriptions.Item>
+				{timeStamp && <Descriptions.Item label={messages.details.timestamp}>{timeStamp}</Descriptions.Item>}
+				{confirmations && (
+					<Descriptions.Item label={messages.details.confirmations}>{confirmations}</Descriptions.Item>
+				)}
+				<Descriptions.Item label={messages.details.from}>{from}</Descriptions.Item>
+				<Descriptions.Item label={messages.details.to}>{to}</Descriptions.Item>
+				<Descriptions.Item label={messages.details.value}>{value}</Descriptions.Item>
+				<Descriptions.Item label={messages.details.gas}>{gas}</Descriptions.Item>
+				<Descriptions.Item label={messages.details.gasprice}>{gasPrice}</Descriptions.Item>
+				<Descriptions.Item label={messages.details.nonce}>{nonce}</Descriptions.Item>
+			</Descriptions>
+		</>
+	);
+};
+
+export default DetailCard;
